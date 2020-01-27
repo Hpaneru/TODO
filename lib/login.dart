2176782d38816a4cd529edd780fbb4e3345f8dd8
HP <hpaneru.hp@yahoo.com>
2020-01-27 11:48:18 +0545

    slightly chnage

--- lib/login.dart
+++ lib/login.dart
@@ -11,82 +11,84 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String _email;
   String _password;
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        body:Center(
-      child: SingleChildScrollView(
-              child: Container(
-              padding: EdgeInsets.all(25.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("LOGIN",
-                   style: TextStyle(
-                    fontSize: 30.0,
-                    color: Colors.blueAccent[700],
-                    fontWeight: FontWeight.bold),
-                      ),
-                  SizedBox(height: 15.0),
-                  TextField(
-                      decoration: InputDecoration(hintText: 'EMAIL'),
-                      onChanged: (value) {
-                        setState(() {
-                          _email = value;
-                        });
-                      }),
-                  SizedBox(height: 15.0),
-                  TextField(
-                    decoration: InputDecoration(hintText: 'PASSWORD'),
-                    onChanged: (value) {
-                      setState(() {
-                        _password = value;
-                      });
-                    },
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 20.0),
-                  RaisedButton(
-                    child: Text('Login'),
-                    color: Colors.blue,
-                    textColor: Colors.white,
-                    elevation: 7.0,
-                    onPressed: () {
-                       FirebaseAuth.instance
-                           .signInWithEmailAndPassword(
-                               email: _email, password: _password)
-                           .then((AuthResult user) {
-                         Navigator.pushReplacement(
-                           context,
-                           MaterialPageRoute(builder: (context)=> ToDo())
-                           );
-                       }).catchError((e) {
-                         print(e);
-                       });
-                    },
-                  ),
-                  SizedBox(height: 15.0),
-                  Text('Don\'t have an account?'),
-                  SizedBox(height: 10.0),
-                  RaisedButton(
-                    child: Text('Sign Up'),
-                    color: Colors.blue,
-                    textColor: Colors.white,
-                    elevation: 7.0,
-                    onPressed: () {
-                       Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (context) => Signup(),
-              )
-            );
-                    },
-                  ),
-                ],
-              )),
+        body: Center(
+            child: SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.all(25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "LOGIN",
+              style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.blueAccent[700],
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 50.0),
+            TextField(
+                decoration: InputDecoration(hintText: 'EMAIL'),
+                onChanged: (value) {
+                  setState(() {
+                    _email = value;
+                  });
+                }),
+            SizedBox(height: 30.0),
+            TextField(
+              decoration: InputDecoration(hintText: 'PASSWORD'),
+              onChanged: (value) {
+                setState(() {
+                  _password = value;
+                });
+              },
+              obscureText: true,
+            ),
+            SizedBox(height: 60.0),
+            FlatButton(
+              child:
+                  loading == true ? CircularProgressIndicator() : Text('Login'),
+              textColor: Colors.blue,
+              onPressed: () {
+                setState(() {
+                  loading = true;
+                });
+                FirebaseAuth.instance
+                    .signInWithEmailAndPassword(
+                        email: _email, password: _password)
+                    .then((AuthResult user) {
+                  Navigator.pushReplacement(
+                      context, MaterialPageRoute(builder: (context) => ToDo()));
+                }).catchError((e) {
+                  print(e);
+                });
+              },
+            ),
+            SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Text('Don\'t have an account?'),
+                FlatButton(
+                  child: Text('Sign Up'),
+                  textColor: Colors.blue,
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Signup(),
+                        ));
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
       ),
-    ),
-        );
+    )));
   }
 }
--- lib/signup.dart
+++ lib/signup.dart
@@ -5,17 +5,18 @@ import 'package:todo/todo.dart';
 import 'services/usermanagement.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
- 
+
 class Signup extends StatefulWidget {
   @override
   _SignupState createState() => _SignupState();
 }
- 
+
 class _SignupState extends State<Signup> {
   File _image;
-  User userInfo=User();
+  User userInfo = User();
   String _password;
- 
+  bool loading = false;
+
   Future getImage() async {
     var image = await ImagePicker.pickImage(source: ImageSource.gallery);
 
@@ -23,47 +24,44 @@ class _SignupState extends State<Signup> {
       _image = image;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-                  child: Center(
-      child: Container(
+      appBar: AppBar(
+        title: Text("SIGNUP"),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
             padding: EdgeInsets.all(25.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text("SIGNUP",
-                 style: TextStyle(
-                  fontSize: 30.0,
-                  color: Colors.blueAccent[700],
-                  fontWeight: FontWeight.bold),
-                    ), SizedBox(height: 15.0),
-                    GestureDetector(
-                      onTap: getImage,
-                      child: Container(
-                          width: 150.0,
-                          height: 150.0,
-                          decoration: BoxDecoration(
-                              color: Colors.red,
-                              image: DecorationImage(
-                                  image:
-                                  _image != null         
-                                     ? FileImage(_image)
-                                      : AssetImage('img/image.png'),
-                                  fit: BoxFit.cover),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(75.0)),
-                              boxShadow: [
-                                BoxShadow(blurRadius: 7.0, color: Colors.black)
-                              ])),
-                    ),
-                 SizedBox(height: 15.0),
+                SizedBox(height: 15.0),
+                GestureDetector(
+                  onTap: getImage,
+                  child: Container(
+                      width: 150.0,
+                      height: 150.0,
+                      decoration: BoxDecoration(
+                          color: Colors.red,
+                          image: DecorationImage(
+                              image: _image != null
+                                  ? FileImage(_image)
+                                  : AssetImage('img/image.png'),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.all(Radius.circular(75.0)),
+                          boxShadow: [
+                            BoxShadow(blurRadius: 7.0, color: Colors.black)
+                          ])),
+                ),
+                SizedBox(height: 15.0),
                 TextField(
                     decoration: InputDecoration(hintText: 'FULL NAME'),
                     onChanged: (value) {
                       setState(() {
-                       userInfo.name = value;
+                        userInfo.name = value;
                       });
                     }),
                 SizedBox(height: 15.0),
@@ -71,7 +69,7 @@ class _SignupState extends State<Signup> {
                     decoration: InputDecoration(hintText: 'EMAIL'),
                     onChanged: (value) {
                       setState(() {
-                        userInfo.email= value;
+                        userInfo.email = value;
                       });
                     }),
                 SizedBox(height: 15.0),
@@ -90,59 +88,60 @@ class _SignupState extends State<Signup> {
                       setState(() {
                         userInfo.address = value;
                       });
-                    }),   
-                 SizedBox(height: 15.0),
+                    }),
+                SizedBox(height: 15.0),
                 TextField(
                     decoration: InputDecoration(hintText: 'PHONE'),
                     onChanged: (value) {
                       setState(() {
                         userInfo.phone = value;
                       });
-                    }),    
+                    }),
                 SizedBox(height: 15.0),
-                RaisedButton(
-                  child: Text('Sign Up'),
-                  color: Colors.blue,
-                  textColor: Colors.white,
-                  elevation: 7.0,
+                FlatButton(
+                  child: loading==true? CircularProgressIndicator(): 
+                  Text('Sign Up'),
+                  textColor: Colors.blue,
                   onPressed: () {
+                    setState(() {
+                      loading = true;
+                    });
                     FirebaseAuth.instance
                         .createUserWithEmailAndPassword(
                             email: userInfo.email, password: _password)
-                        .then((signedInUser) async{
-                         
-                      userInfo.id =   signedInUser.user.uid;
-                          
-                      var imageUrl = await uploadFile(userInfo.id); 
+                        .then((signedInUser) async {
+                      userInfo.id = signedInUser.user.uid;
+
+                      var imageUrl = await uploadFile(userInfo.id);
                       userInfo.imageUrl = imageUrl;
 
                       UserManagement().storeNewUser(userInfo, context);
-                        Navigator.pushReplacement(
-                         context,
-                         MaterialPageRoute(builder: (context)=> ToDo())
-                         );
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) => ToDo()));
                     }).catchError((e) {
                       print(e);
                     });
                   },
                 ),
               ],
-            )),
-    ),
-        ));
+            ),
+          ),
+        ),
+      ),
+    );
   }
-  Future<String> uploadFile(String uid) async{
-    StorageReference storageReference = FirebaseStorage.instance
-    .ref()
-    .child('ProfilePictures/$uid');
+
+  Future<String> uploadFile(String uid) async {
+    StorageReference storageReference =
+        FirebaseStorage.instance.ref().child('ProfilePictures/$uid');
     StorageUploadTask uploadTask = storageReference.putFile(_image);
     await uploadTask.onComplete;
-    print("fileeeeeeeeeeee uploadedddddddddddd");
     String imageUrl = await storageReference.getDownloadURL();
     return imageUrl;
   }
 }
-class User{
+
+class User {
   String id;
   String email;
   String name;
@@ -150,5 +149,11 @@ class User{
   String phone;
   String imageUrl;
 
-  User({this.id, this.email, this.name, this.address, this.phone, this.imageUrl});
+  User(
+      {this.id,
+      this.email,
+      this.name,
+      this.address,
+      this.phone,
+      this.imageUrl});
 }
